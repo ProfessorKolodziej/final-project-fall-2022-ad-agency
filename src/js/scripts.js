7d35175c8aa33683ecf4be177f487e3fe7218279
timwu11 <timwu@bu.edu>
2022-12-06 15:13:17 -0500

    more
    Signed-off-by: timwu11 <timwu@bu.edu>

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,27 +2,30 @@ const url = 'https://api.baserow.io/api/database/rows/table/115440/?user_field_n
 
 const list = document.querySelector('#list');
 let storedData = null;
+function buildHTML(data) {
+	// eslint-disable-next-line
+	for (let i = 0; i < data.length; i++) {
+		console.log(data[i]);
+
+		const markup = `<li>
+<img class="Logos" src = ${data[i].Logos[0].url}>
+<h2>${data[i]['Agency Name']}</h2> 
+<div class="Locations">${data[i].Locations}</div>
+</li>`;
+
+		list.insertAdjacentHTML('beforeend', markup);
+	}};
 async function getData() {
 	await fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
 		.then((response) => response.json())
 		.then((data) => {
 			storedData = data.results;
-			// eslint-disable-next-line
-		for (let i = 0; i < storedData.length; i++) {
-				console.log(storedData[i]);
-
-				const markup = `<li>
-        <img class="Logos" src = ${storedData[i].Logos[0].url}>
-        <h2>${storedData[i]['Agency Name']}</h2> 
-        <div class="Locations">${storedData[i].Locations}</div>
-      </li>`;
-
-				list.insertAdjacentHTML('beforeend', markup);
-			}
+buildHTML(storedData);
 		});
 }
 // eslint-disable-next-line
 getData().then((result) => {
-	const filtered = storedData.filter((item) => item['Agency Name'] === 'BBDO');
-	console.log(filtered);
+	const filtered = storedData.filter((item) => item['Locations'].includes("Boston"));
+	buildHTML(filtered); 
+	console.log(filtered); 
 });
