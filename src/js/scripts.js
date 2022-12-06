855620e258df36f944b566892bf3f8066d043c91
timwu11 <timwu@bu.edu>
2022-12-06 11:47:40 -0500

    OH changes 2
    Signed-off-by: timwu11 <timwu@bu.edu>

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,20 +1,28 @@
 const url = 'https://api.baserow.io/api/database/rows/table/115440/?user_field_names=true';
 
 const list = document.querySelector('#list');
+let storedData = null;
+async function getData() {
+	await fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
+		.then((response) => response.json())
+		.then((data) => {
+			storedData = data.results;
+			// eslint-disable-next-line
+		for (let i = 0; i < storedData.length; i++) {
+				console.log(storedData[i]);
 
-fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
-	.then((response) => response.json())
-	.then((data) => {
-		// eslint-disable-next-line
-		for (let i = 0; i < data.results.length; i++) {
-			console.log(data.results[i]);
-
-			const markup = `<li>
-        <img class="Logos" src = ${data.results[i].Logos[0].url}>
-        <h2>${data.results[i]['Agency Name']}</h2> 
-        <div class="Locations">${data.results[i].Locations}</div>
+				const markup = `<li>
+        <img class="Logos" src = ${storedData[i].Logos[0].url}>
+        <h2>${storedData[i]['Agency Name']}</h2> 
+        <div class="Locations">${storedData[i].Locations}</div>
       </li>`;
 
-			list.insertAdjacentHTML('beforeend', markup);
-		}
-	});
+				list.insertAdjacentHTML('beforeend', markup);
+			}
+		});
+}
+// eslint-disable-next-line
+getData().then((result) => {
+	const filtered = storedData.filter((item) => item['Agency Name'] === 'BBDO');
+	console.log(filtered);
+});
