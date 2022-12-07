9269142af76a74c402071587cf7b15d5bd7f9de4
Ashley Kolodziej <likeapenguininthedark@gmail.com>
2022-12-07 12:31:25 -0500

    Comments and fixing :)

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,82 +1,52 @@
-const { run } = require('jest');
-
 const url = 'https://api.baserow.io/api/database/rows/table/115440/?user_field_names=true';
 
 const list = document.querySelector('#list');
 let storedData = null;
+
 function buildHTML(data) {
+	let markup = null;
+
 	// eslint-disable-next-line
 	for (let i = 0; i < data.length; i++) {
-		console.log(data[i]);
-
-
-		const markup = `<li>
-<img class="Logos" src = ${data[i].Logos[0].url}>
-<h2>${data[i]['Agency Name']}</h2> 
-<div class="Locations">${data[i].Locations}</div>
-</li>`;
-
-		list.insertAdjacentHTML('beforeend', markup);
+		// console.log(data[i]);
+		markup += `<li>
+			<img class="Logos" src = ${data[i].Logos[0].url}>
+			<h2>${data[i]['Agency Name']}</h2>
+			<div class="Locations">${data[i].Locations}</div>
+			</li>`;
 	}
+
+	list.innerHTML = markup;
 }
+
+// This is good if you want to add a reset button.
+// Add the button the same way as buildBoston is added.
+function buildAll() {
+	buildHTML(storedData);
+}
+
+// This gets the initial dataset and builds the results with it.
 async function getData() {
 	await fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
 		.then((response) => response.json())
 		.then((data) => {
 			storedData = data.results;
+			buildAll(storedData);
 		});
 }
-getData().then((result) => {
+
+// This builds the list using the Boston filter.
+// It's another way of writing a function - same as line 24.
+// You can use either style, whichever you like best.
+// This is more modern, but it can be confusing when you're learning.
+const buildBoston = () => {
 	const filtered = storedData.filter((item) => item.Locations.includes('Boston'));
+	// console.log(filtered);
 	buildHTML(filtered);
-	console.log(filtered);
-});
-
-
-//let onclick = "buildBoston"() => {
-
-let onclick = "buildBoston"() => {
-	getData(storedData.filtered)
 };
 
-let x = "all";
+const buildBostonButton = document.querySelector('#buildBoston');
+buildBostonButton.addEventListener('click', buildBoston);
 
-let buildAll = () => {
-	buildHTML(storedData);
-	console.log(storedData);
-
-	let changeBoston = () => {
-		x = "boston";
-	}}
-
-let buildBoston = () => {
-	const filtered = storedData.filter((item) => item.Locations.includes('Boston'));
-	buildHTML(filtered);
-	console.log(filtered);
-}
-
-function build(){
-	if (x == "all"){
-		buildAll();
-	}
-	then {
-		buildBoston();
-	}
-}
-
-// eslint-disable-next-line
-fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
-	.then((response) => response.json())
-	.then((data) => {
-		for (let i = 0; i < data.results.length; i++) {
-			console.log(data.results[i]);
-
-			const markup = `<li>
-        <img class="Logos" src = ${data.results[i].Logos[0].url}>
-        <h2>${data.results[i]['Agency Name']}</h2> 
-        <div class="Locations">${data.results[i].Locations}</div>
-      </li>`;
-
-			list.insertAdjacentHTML('beforeend', markup);
-		}
-	});
+// Build the initial list when the page loads.
+getData();
