1466196c6b29889feabdc913518318c5d9af142b
ngeary113 <ngeary@bu.edu>
2022-12-06 19:32:25 -0500

    More CSS
    CSS update

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,18 +2,18 @@ const url = 'https://api.baserow.io/api/database/rows/table/115440/?user_field_n
 
 const list = document.querySelector('#list');
 
-fetch(url, { headers: { Authorization: `Token woLMVJy43hZwBu44UBVYVabQyulxwTOw` } })
-  .then(response => response.json())
-  .then(function(data) {
-    for ( let i = 0; i < data.results.length; i++ ) {
-      console.log(data.results[i]);
-      
-      const markup = `<li>
-        <img class="Logos" src = ${data.results[i]["Logos"][0].url}>
-        <h2>${data.results[i]["Agency Name"]}</h2> 
-        <div class="Locations">${data.results[i]["Locations"]}</div>
-      </li>`
-      
-       list.insertAdjacentHTML('beforeend', markup);
-    }
-});
+fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
+	.then((response) => response.json())
+	.then((data) => {
+		for (let i = 0; i < data.results.length; i++) {
+			console.log(data.results[i]);
+
+			const markup = `<li>
+        <img class="Logos" src = ${data.results[i].Logos[0].url}>
+        <h2>${data.results[i]['Agency Name']}</h2> 
+        <div class="Locations">${data.results[i].Locations}</div>
+      </li>`;
+
+			list.insertAdjacentHTML('beforeend', markup);
+		}
+	});
