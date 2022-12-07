cb006875a54ec24a08af76a90f02034f19319eab
timwu11 <timwu@bu.edu>
2022-12-07 00:06:22 -0500

    ,.,
    Signed-off-by: timwu11 <timwu@bu.edu>

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,19 +1,67 @@
+const { run } = require('jest');
+
 const url = 'https://api.baserow.io/api/database/rows/table/115440/?user_field_names=true';
 
 const list = document.querySelector('#list');
+let storedData = null;
+function buildHTML(data) {
+	// eslint-disable-next-line
+	for (let i = 0; i < data.length; i++) {
+		console.log(data[i]);
 
-fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
-	.then((response) => response.json())
-	.then((data) => {
-		for (let i = 0; i < data.results.length; i++) {
-			console.log(data.results[i]);
+		const markup = `<li>
+<img class="Logos" src = ${data[i].Logos[0].url}>
+<h2>${data[i]['Agency Name']}</h2> 
+<div class="Locations">${data[i].Locations}</div>
+</li>`;
 
-			const markup = `<li>
-        <img class="Logos" src = ${data.results[i].Logos[0].url}>
-        <h2>${data.results[i]['Agency Name']}</h2> 
-        <div class="Locations">${data.results[i].Locations}</div>
-      </li>`;
+		list.insertAdjacentHTML('beforeend', markup);
+	}
+}
+async function getData() {
+	await fetch(url, { headers: { Authorization: 'Token woLMVJy43hZwBu44UBVYVabQyulxwTOw' } })
+		.then((response) => response.json())
+		.then((data) => {
+			storedData = data.results;
+			// buildHTML(storedData);
+			return storedData;
+		});
+}
+/*
+let x = "all";
 
-			list.insertAdjacentHTML('beforeend', markup);
-		}
-	});
+let changeBoston = () => {
+	x = "boston";
+}
+
+let buildBoston = () => {
+	const filtered = storedData.filter((item) => item.Locations.includes('Boston'));
+	buildHTML(filtered);
+	console.log(filtered);
+}
+
+let buildAll = () => {
+	buildHTML(storedData);
+	console.log(storedData);
+}
+
+function build(){
+	if (x == "all"){
+		buildAll();
+	}
+	else{
+		buildBoston();
+	}
+}
+*/
+// eslint-disable-next-line
+
+// onclick = buildBoston()
+getData().then((result) => {
+	const filtered = storedData.filter((item) => item.Locations.includes('Boston'));
+	buildHTML(filtered);
+	console.log(filtered);
+});
+
+// <input type="button">
+// <button>
